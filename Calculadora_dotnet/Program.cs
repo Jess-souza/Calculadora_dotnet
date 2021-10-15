0f74df00ebc157e26c6feae35f6f06bb4ac82448
Jéssica Souza <78058522+Jess-souza@users.noreply.github.com>
2021-10-15 17:33:34 -0300

    Atualizei a Branch

--- Calculadora_dotnet/Program.cs
+++ Calculadora_dotnet/Program.cs
@@ -6,7 +6,19 @@ namespace Calculadora_dotnet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double n1;
+            double n2;
+            double resultado;
+
+            Console.WriteLine("Digite o primeiro número: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            resultado = n1 + n2;
+            Console.WriteLine("O resultado da minha soma é: {0}", resultado);
+
+
         }
     }
 }
