4f333fa032616e0c6aca3c40f0c2aea6ebdd0080
Jéssica Souza <78058522+Jess-souza@users.noreply.github.com>
2021-10-15 18:44:25 -0300

    Calculadora concluída

--- Calculadora_dotnet/Program.cs
+++ Calculadora_dotnet/Program.cs
@@ -6,19 +6,98 @@ namespace Calculadora_dotnet
     {
         static void Main(string[] args)
         {
+            int opcao;
             double n1;
             double n2;
             double resultado;
+            bool validacao = true;
+            Program program = new Program();
 
-            Console.WriteLine("Digite o primeiro número: ");
-            n1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Digite o segundo número: ");
-            n2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("###Sejam Bem vindos a calculadora DOTNET###");
+            
+            do
+            {
+                Console.WriteLine("Qual a operação desejada?");
+                Console.WriteLine("(1) Soma");
+                Console.WriteLine("(2) Subtração");
+                Console.WriteLine("(3) Multiplicação");
+                Console.WriteLine("(4) Divisão");
+                Console.WriteLine("(5) Sair");
 
-            resultado = n1 + n2;
-            Console.WriteLine("O resultado da minha soma é: {0}", resultado);
+                Console.WriteLine("Digite a sua opção: ");
+                opcao = Convert.ToInt32(Console.ReadLine());
 
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine("Digite o primeiro número: ");
+                        n1 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Digite o segundo número: ");
+                        n2 = Convert.ToDouble(Console.ReadLine());
 
+                        resultado = program.Soma(n1, n2);
+                        Console.WriteLine("O resultado da minha soma é: {0}", resultado);
+                        break;
+                    case 2:
+                        Console.WriteLine("Digite o primeiro número: ");
+                        n1 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Digite o segundo número: ");
+                        n2 = Convert.ToDouble(Console.ReadLine());
+
+                        resultado = program.Subtracao(n1, n2);
+                        Console.WriteLine("O resultado da minha subtração é: {0}", resultado);
+                        break;
+                    case 3:
+                        Console.WriteLine("Digite o primeiro número: ");
+                        n1 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Digite o segundo número: ");
+                        n2 = Convert.ToDouble(Console.ReadLine());
+
+                        resultado = program.Multiplicacao(n1, n2);
+                        Console.WriteLine("O resultado da minha multiplicação é: {0}", resultado);
+                        break;
+                    case 4:
+                        Console.WriteLine("Digite o primeiro número: ");
+                        n1 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Digite o segundo número: ");
+                        n2 = Convert.ToDouble(Console.ReadLine());
+
+                        resultado = program.Divisao(n1, n2);
+                        Console.WriteLine("O resultado da minha divisão é: {0}", resultado);
+                        break;
+                    case 5:
+                        validacao = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opção Inválida");
+                        break;
+                }
+
+                Console.WriteLine("Aperte Enter para continuar");
+                Console.ReadLine();
+                Console.Clear();
+            } while (validacao);
+            {              
+            }
+        }
+        public double Soma(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+
+        public double Subtracao(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+
+        public double Multiplicacao(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+        
+        public double Divisao(double n1, double n2)
+        {
+            return n1 / n2;
         }
     }
 }
